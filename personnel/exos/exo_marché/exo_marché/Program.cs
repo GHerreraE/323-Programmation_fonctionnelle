decdd487a3d69d4cef13d644664763ba0316e46f
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-08-27 11:18:52 +0200

    feat(exos): Exercice Place du marché

---
+++ personnel/exos/exo_marché/exo_marché/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronXL;
+
+namespace exo_marché
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // On charge le fichier excel
+            WorkBook workBook = WorkBook.Load(@"C:\Users\py78dal\Documents\GitHub\323-Programmation_fonctionnelle\personnel\exos\marché\Place du marché.xlsx");
+
+            // On select la feuille
+            WorkSheet workSheet = workBook.WorkSheets[1];
+
+
+            // TEST AFFICHAGE
+            foreach (var cell in workSheet["A10:A20"])
+            {
+                Console.WriteLine("Cell {0} has value '{1}'", cell.AddressString, cell.Text);
+                
+            }
+
+            // Combien y a-t-il de vendeurs de pêche dans ce marché ?
+
+            workSheet["C1:C100"].Value = "Pêches";
+
+
+            foreach (var row in workSheet.Rows)
+            {
+                foreach (var col in row)
+                {
+                    Console.WriteLine(col.Value);
+                }
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ personnel/mes notes/notes.md
@@ -0,0 +1,21 @@
+# 27/08/2025
+
+### Impératif
+- C'est genre une recette de cuisine pas à pas.
+- Prend plus de temps mais c'est plus detaillé.
+
+### Déclaratif 
+- C'et genre une recette de cuisine simplifié. 
+- C'est plus rapide mais, si on veut faire des conditions il faut faire plusieurs trucs. 
+- La syntaxe se raseemble à Mongo.
+
+    ![alt text](image.png)
+- #### VAR
+    - C'est pour déclarer une valeur de n'importe quel type. C'est pour faire une fonction.
+
+### Fonctions supplémentaires
+
+- #### Fonctions -> Au lieu de faire une fonction en syntaxe de méthode comment on connait déjà, on commencera à faire avec ACTION (en une seule ligne). 
+    - On peut appeler une fonction et utiliser un commentaire
+        ![alt text](image-1.png)
+        
